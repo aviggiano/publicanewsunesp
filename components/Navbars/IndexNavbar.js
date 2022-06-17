d519dc0bd13a6ca06db8631ef857dbb22b9add74
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2022-06-17 15:50:55 -0300

    Dynamic projects WIP

--- components/Navbars/IndexNavbar.js
+++ components/Navbars/IndexNavbar.js
@@ -70,7 +70,9 @@ export default function Navbar(props) {
                   className="bg-blueGray-700 text-white active:bg-blueGray-600 text-xs font-bold uppercase px-4 py-2 rounded shadow hover:shadow-lg outline-none focus:outline-none lg:mr-1 lg:mb-0 ml-3 mb-3 ease-linear transition-all duration-150"
                   type="button"
                 >
-                  <i className="fas fa-arrow-alt-circle-down"></i> Download
+                  <a href="https://airtable.com/shrswvtHV2tzfY3Nl" target="_blank" noreferrer rel="noreferrer">
+                    Envie o seu projeto
+                  </a>
                 </button>
               </li>
             </ul>
